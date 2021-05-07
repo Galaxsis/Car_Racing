6a66d421757e8973f92883e4feb402a616611de5
Galaxsis <78539819+Galaxsis@users.noreply.github.com>
2021-05-07 17:06:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,47 +9,43 @@ car1_y= 10;
 
 car2_width= 120;
 car2_height= 70;
-car2_image= "car2.jpg";
+car2_image= "car2.png";
 car2_x= 10;
 car2_y= 100;
 
-background_image= "racing track.jpg";
+background_image = "racing track.jpg";
 
-function add_car1() {
-background_imgTag= new Image();
-background_imgTag.onload = uploadbackground;
-background_imgTag.src = background_image;
+function add_car() {
+	background_imgTag = new Image();
+	background_imgTag.onload = uploadBackground;
+	background_imgTag.src = background_image;
 
-car1_imgTag= new Image();
-car1_imgTag.onload = uploadcar1;
-car1_imgTag.src = car1_image;
-}
-
-function add_car2() {
-    background_imgTag= new Image();
-    background_imgTag.onload = uploadbackground;
-    background_imgTag.src = background_image;
+	car1_imgTag= new Image();
+	car1_imgTag.onload = uploadcar1;
+	car1_imgTag.src = car1_image;
     
-    car2_imgTag= new Image();
-    car2_imgTag.onload = uploadcar2;
-    car2_imgTag.src = car2_image;
-    }
+    	car2_imgTag= new Image();
+	car2_imgTag.onload = uploadcar2;
+	car2_imgTag.src = car2_image;
+}
 
-function uploadbackground() {
-  ctx.drawingImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+function uploadBackground() {
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 }
+
 function uploadcar1() {
-   ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);  
+   	ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);  
 }
+
 function uploadcar2() {
-     ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);  
-  }
+	ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);  
+}
   
 window.addEventListener("keydown", my_keydown);
 
 function my_keydown(e)
 {
-    keyPressed= e.keyCode;
+    keyPressed = e.keyCode;
     console.log(keyPressed);
     if(keyPressed == '38')
     {
